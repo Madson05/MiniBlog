abeb1218829743ba1583bd636f484acc5591b77b
madson <madsonsillva2005@gmail.com>
2022-12-18 18:22:09 -0300

    vinculado o firebase ao projeto

---
+++ firebase/config.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD2-kcC4MPzppPuutXa-4fCdxtwaGA0pFo",
+  authDomain: "miniblog-18de6.firebaseapp.com",
+  projectId: "miniblog-18de6",
+  storageBucket: "miniblog-18de6.appspot.com",
+  messagingSenderId: "457118240840",
+  appId: "1:457118240840:web:d9a3a9439135f8dd62487d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore;
+
+export {db};
