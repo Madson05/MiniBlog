bc8f2a0b6c3f8bf16eb1b8541be3be0c2548a982
madson <madsonsillva2005@gmail.com>
2022-12-18 20:28:33 -0300

    importado react-router e criado as primeiras rotas do projeto

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 
 import './App.css';
 
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom"
+
+// pages
+import About from './pages/About/About';
+import Home from './pages/Home/Home';
+
 function App() {
   return (
     <div className="App">
-      <h1>MiniBlog</h1>
+      
+
+      <BrowserRouter>
+        <div className="container">
+          <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/about' element={<About/>}/>
+
+          </Routes>
+        </div>
+      </BrowserRouter>
 
     </div>
   );
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,11 @@
+import styles from "./Home.module.css"
+
+const Home = () => {
+  return (
+    <div>
+        <h1>home</h1>
+    </div>
+  )
+}
+
+export default Home
