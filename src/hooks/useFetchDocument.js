b411914f6758f035bdc18e84088b77a3038fa417
madson <madsonsilva2005@gmail.com>
2022-12-27 15:54:53 -0300

    criado o hook de resgate de dados do firebase

---
+++ src/hooks/useFetchDocument.js
@@ -0,0 +1,67 @@
+import { useState, useEffect } from "react";
+import { db } from "../firebase/config"
+import { collection, query, orderBy, Onsnapshot, where, QuerySnapshot } from "firebase/firestore";
+
+
+
+export const useFetchDocuments = (docCollection, search = null, uid = null) => {  
+
+    const [documents, setDocuments] = useState(null)
+    const [error, setError] = useState(null)
+    const [loading, setLoading] = useState(null)
+
+    // deal with memory leak
+    const [cancelled, setCancelled] = useState(false);
+
+
+    useEffect(()=>{
+
+        async function loadData(){
+            if(cancelled) return
+
+            setLoading(true)
+
+            const collectionRef = await collection(db, docCollection)
+
+            try{
+                let q;
+
+                // busca
+                // dashboard
+
+                q = await query(collectionRef, orderBy("CreatedAt", "desc"))
+
+                await Onsnapshot(q,  (QuerySnapshot) => {
+
+
+                    setDocuments(
+                        QuerySnapshot.docs.map((doc) => {
+                            id: doc.id,
+                            {...doc.data()}
+
+                    })
+                    )
+                })
+
+                setLoading(false)
+
+            }catch(error){
+                console.log(error);
+                setError(error.message);
+                setLoading(false)
+
+            }    
+        }
+
+        loadData()
+
+    }, [docCollection, search, uid, cancelled])
+
+
+    useEffect(() => {
+        return () => setCancelled(true);
+    }, [])
+
+    return { documents, loading, error };
+
+}
