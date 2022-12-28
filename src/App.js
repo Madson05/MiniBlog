615553694083faf25dc9205be9281dfb1a06fb51
madson <madsonsilva2005@gmail.com>
2022-12-28 15:06:26 -0300

    criada pagina de post individual

--- src/App.js
+++ src/App.js
@@ -23,10 +23,12 @@ import Register from './pages/Register/Register';
 import Dashboard from "./pages/Dashboard/Dashboard";
 import CreatePost from "./pages/CreatePost/CreatePost";
 import Search from "./pages/Search/Search";
+import Post from "./pages/Post/Post";
 
 // components
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
+
 
 
 
@@ -62,6 +64,7 @@ function App() {
               <Route path='/' element={<Home/>}/>
               <Route path='/search' element={<Search/>}/>
               <Route path='/about' element={<About/>}/>
+              <Route path='/posts/:id' element={<Post/>}/>
               <Route path='/login' element={!user? <Login/>: <Navigate to = "/"/>}/>
               <Route path='/register' element={!user? <Register/>: <Navigate to = "/"/>}/>
               <Route path='/dashboard' element={user? <Dashboard/>: <Navigate to = "/login"/>}/>
---
+++ src/pages/Post/Post.js
@@ -0,0 +1,15 @@
+import styles from "./Post.module.css"
+
+// hooks
+import { useParams } from "react-router-dom"
+
+const Post = () => {
+  const {id} = useParams()
+  return (
+    <div>
+        <h1>Post {id}</h1>
+    </div>
+  )
+}
+
+export default Post
