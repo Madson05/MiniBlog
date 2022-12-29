fbd6bced72d8bd5df2ea10130b7e1b2fa481bc18
madson <madsonsilva2005@gmail.com>
2022-12-28 22:30:01 -0300

    dashboard estruturado

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,9 +1,33 @@
 import styles from "./Dashboard.module.css"
 
+import { Link } from "react-router-dom"
+
+// hooks
+
+import {useAuthValue} from "../../context/AuthContext";
+import { useFetchDocuments } from "../../hooks/useFetchDocument";
+
 const Dashboard = () => {
+
+  const user = useAuthValue()
+  const uid = user.uid;
+
+  // posts do usuario
+
+  const posts = []
+
   return (
     <div>
-        <h1>dashboard</h1>
+        <h2>Dashboard</h2>
+        <p>Gerencie os seus posts</p>
+        {posts && posts.length === 0 && (
+          <div>
+            <p>Não foram encontrados posts</p>
+
+            <Link to = "posts/create" className="btn">Criar primeiro post</Link>
+          </div>
+          
+        )}
     </div>
   )
 }
