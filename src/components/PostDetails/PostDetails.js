0a33f9fd7f476bcfd575a75343e4de5f69e4d91b
madson <madsonsilva2005@gmail.com>
2022-12-29 17:44:29 -0300

    corrigido erro nas tags

--- src/components/PostDetails/PostDetails.js
+++ src/components/PostDetails/PostDetails.js
@@ -1,21 +1,25 @@
 import styles from "./PostDetails.module.css"
 import { Link } from "react-router-dom"
 
-const PostDetails = ({post}) => {
+const PostDetails = ({ post }) => {
   return (
-    <div key = {post.id}className={styles.Post_details}>
-        <img src={post.image} alt={post.title} />
-        <h2>{post.title}</h2>
-        <p className={styles.createdBy}>{post.createdBy}</p>
-        <div className={styles.tags}>
-            {post.tags.map((tag)=>(
-                <p key={tag.id}><span>#</span>{tag}</p>
-            ))}
-        </div>
-
-        <Link to={`/posts/${post.id}`} className="btn btn-outline">Ler</Link>
+    <div className={styles.post_details}>
+      <img src={post.image} alt={post.title} />
+      <h2>{post.title}</h2>
+      <p className={styles.createdBy}>por: {post.createdBy}</p>
+      <div className={styles.tags}>
+        {post.tags.map((tag) => (
+          <p key={tag}>
+            <span>#</span>
+            {tag}
+          </p>
+        ))}
+      </div>
+      <Link to={`/posts/${post.id}`} className="btn btn-outline">
+        Ler
+      </Link>
     </div>
-  )
-}
+  );
+};
 
 export default PostDetails
