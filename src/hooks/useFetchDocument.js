8bdf493fc0246c8895c05efb9253c9faf09c108a
madson <madsonsilva2005@gmail.com>
2022-12-28 21:57:26 -0300

    ciado hook de exibição de post individual

---
+++ src/hooks/useFetchDocument.js
@@ -0,0 +1,53 @@
+import { useState, useEffect } from "react";
+import { db } from "../firebase/config";
+import {
+  doc, 
+  getDoc
+} from "firebase/firestore";
+
+export const useFetchDocument = (docCollection, id) => {
+  const [document, setDocument] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(null);
+
+  // deal with memory leak
+  const [cancelled, setCancelled] = useState(false);
+
+  useEffect(() => {
+    async function loadDocument() {
+      if (cancelled) {
+        return;
+      }
+
+      setLoading(true);
+
+
+      try{
+        const docRef = await doc(db, docCollection, id);
+        const docSnap = await getDoc(docRef)
+
+        setDocument(docSnap.data());
+
+        setLoading(false)
+
+      }catch(error){
+        setError(error.message);
+        setLoading(true)
+
+        
+
+      }
+
+    }
+
+    loadDocument();
+  }, [docCollection, id]);
+
+  console.log(documents);
+
+  useEffect(() => {
+    return () => setCancelled(true);
+  }, []);
+
+  return { document, loading, error };
+};
