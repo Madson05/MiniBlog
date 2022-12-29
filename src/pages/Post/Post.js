fb4aa1bb4f68b556f30e88f7177601d53c65f5ed
madson <madsonsilva2005@gmail.com>
2022-12-28 22:19:20 -0300

    pagina de post individual estilizada

--- src/pages/Post/Post.js
+++ src/pages/Post/Post.js
@@ -8,12 +8,20 @@ const Post = () => {
   const {id} = useParams()
   const {document: post, loading} = useFetchDocument("posts", id)
   return (
-    <div>
+    <div className={styles.post_container}>
 
         {loading && <p>Carregando post...</p>}
         {post && (
           <>
             <h1>{post.title}</h1>
+            <img src={post.image} alt={post.title} />
+            <p>{post.body}</p>
+            <h3>Este post trata sobre: </h3>
+            <div className={styles.tags}>
+            {post.tags.map((tag) => (
+              <p key={tag.id}><span>#</span>{tag} </p>
+            ))}
+            </div>
           </>
         )}
     </div>
