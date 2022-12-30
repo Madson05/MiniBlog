35572a7448667679ff461fd119fab474127bcbe2
madson <madsonsilva2005@gmail.com>
2022-12-29 21:22:36 -0300

    corrigido erro no link do sobre

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -6,7 +6,7 @@ const About = () => {
     <div className={styles.about}>
         <h2 clas>Sobre o Mini <span>Blog</span></h2>
         <p>Este projeto consiste em um blog feito com React no front-end e firebase no back-end.</p>
-        <Link to = "/post/create" className = "btn">Criar post</Link>
+        <Link to = "/posts/create" className = "btn">Criar post</Link>
     </div>
   )
 }
